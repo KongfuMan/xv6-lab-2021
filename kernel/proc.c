631733cc32e7de27beb6387e0c23999ea11d65ac
liang <chenliangsjtu@gmail.com>
2022-05-22 16:17:13 -0700

    finish speedup syscall

--- kernel/proc.c
+++ kernel/proc.c
@@ -127,6 +127,16 @@ found:
     return 0;
   }
 
+  if ((p->usyscall = (struct usyscall*)kalloc()) == 0){
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+
+  #ifdef LAB_PGTBL
+    p->usyscall->pid = p->pid;
+  #endif
+
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -196,6 +206,10 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
+  #ifdef LAB_PGTBL
+    mappages(pagetable, USYSCALL, PGSIZE, (uint64)(p->usyscall), PTE_R | PTE_U);
+  #endif
+
   return pagetable;
 }
 
@@ -206,6 +220,9 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  #ifdef LAB_PGTBL
+    uvmunmap(pagetable, USYSCALL, 1, 0);
+  #endif
   uvmfree(pagetable, sz);
 }
 
